e7d29852f795a79f03eeac928e58eac23cce28ff
ermesand <dafka100@gmail.com>
2023-09-07 22:26:02 +0300

    change: прохождение тестов

--- app.js
+++ app.js
@@ -24,4 +24,8 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'страница не найдена' });
+});
+
 app.listen(PORT);
