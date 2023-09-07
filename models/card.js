b2709fcbdd5f4db0d9a2add1b7740bc9ca416f0f
ermesand <dafka100@gmail.com>
2023-09-07 22:14:13 +0300

    change: прохождение тестов

--- models/card.js
+++ models/card.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'заполните поле'],
+    required: [true, 'заполните поле'],
     minlength: [2, 'длина поля должна быть больше 2'],
     maxlength: [30, 'длина поля должна быть не больше 30'],
   },
   link: {
     type: String,
-    require: [true, 'заполните поле'],
+    required: [true, 'заполните поле'],
     validare: {
       validator(v) {
         return /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/.test(v);
@@ -20,7 +20,7 @@ const cardSchema = new mongoose.Schema({
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
-    require: true,
+    required: true,
   },
   likes: {
     type: mongoose.Schema.Types.ObjectId,
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'заполните поле'],
+    required: [true, 'заполните поле'],
     minlength: [2, 'длина поля должна быть больше 2'],
     maxlength: [30, 'длина поля должна быть не больше 30'],
   },
@@ -11,11 +11,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: [2, 'длина поля должна быть больше 2'],
     maxlength: [30, 'длина поля должна быть не больше 30'],
-    require: [true, 'заполните поле'],
+    required: [true, 'заполните поле'],
   },
   avatar: {
     type: String,
-    require: [true, 'заполните поле'],
+    required: [true, 'заполните поле'],
     validare: {
       validator(v) {
         return /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/.test(v);
