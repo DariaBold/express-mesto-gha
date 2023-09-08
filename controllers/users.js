8fd9b5b8d9e01ac71a75d428ebbdca779632bfb2
ermesand <dafka100@gmail.com>
2023-09-08 04:13:46 +0300

    change: прохождение тестов2

--- controllers/users.js
+++ controllers/users.js
@@ -20,12 +20,12 @@ module.exports.getUsers = (req, res) => {
 };
 module.exports.getUsersId = (req, res) => {
   User.findById(req.params.userId)
-    .orFail(new Error('CastError'))
+    .orFail(new Error('NotValidId'))
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (req.params.userId.length !== 24) {
         res.status(400).send({ message: 'Некорректный _id.' });
-      } else if (err.message === 'CastError') {
+      } else if (err.message === 'NotValidId') {
         res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
       } else {
         res.status(500).send({ message: 'На сервере произошла ошибка' });
